e1736a6283abca3ad4e21aa603044894ba8a1861
guoxiaobo <guoxiaobo@zhuanzhuan.com>
2023-09-12 19:08:54 +0800

    feat: 图片记录优化

--- popup/index.tsx
+++ popup/index.tsx
@@ -1,10 +1,12 @@
-import { Col, Form, List, Row, Switch } from "antd"
+import { useEffect, useState } from "react"
+import { Badge, Col, Form, Row, Switch } from "antd"
 import { SettingOutlined } from "@ant-design/icons"
 import { useStorage } from "@plasmohq/storage/hook"
+import { fileSave } from 'browser-fs-access'
+import { useDebounceFn } from "ahooks"
 
 import "antd/dist/reset.css"
 import './index.less'
-import { useDebounceFn } from "ahooks"
 
 import pkg from './../package.json'
 
@@ -17,6 +19,8 @@ export interface IImageUploadConfig {
 
 function IndexPopup() {
   const [form] = Form.useForm()
+  const [hasNewVersion, setHasNewVersion] = useState(false);
+  
   const [imageUploadConfig, setImageUploadConfig] = useStorage<IImageUploadConfig>(
     "image_upload_config", 
     (v) => {
@@ -41,6 +45,18 @@ function IndexPopup() {
   }, {
     wait: 500
   })
+
+  useEffect(() => {
+    fetch('https://feconf.zhuanzhuan.com/feconf/u?keys=chrome-extension-lanhu')
+      .then(res => res.json())
+      .then(res => {
+        const { respData } = res
+
+        if (respData.version && respData.version !== pkg.version) {
+          setHasNewVersion(true)
+        }
+      })
+  }, [])
 
   return (
     <>
@@ -91,9 +107,33 @@ function IndexPopup() {
         </Col>
 
         <Col style={{ cursor: 'pointer' }}>
-          v{pkg.version}
-          {/* <SettingOutlined />
-          <span style={{ marginLeft: 6 }}>设置</span> */}
+          <span>v{pkg.version}</span>
+          {/* <SettingOutlined /> */}
+          {
+            hasNewVersion
+              ? <Badge dot>
+                  <a
+                    style={{ marginLeft: 6 }}
+                    onClick={async () => {
+                      try {
+                        const response = await fetch('https://dl.zhuanstatic.com/fecommon/chrome-mv3-prod.zip', {
+                          method: 'GET',
+                        })
+                        const zipBlob = await response.blob()
+          
+                        await fileSave(
+                          zipBlob,
+                          {
+                            fileName: 'chrome-extension-lanhu'
+                          }
+                        )
+                      } catch (err) {
+                        throw err
+                      }
+                  }}>有更新</a>
+                </Badge>
+              : null
+          }
         </Col>
       </Row>
     </>
